4d01560508f0e58c698016e8b3c736c133cf33a3
Jonathan Perry <jonathan.perry@ziffdavis.com>
2023-08-25 12:30:21 -0500

    Finish part 2

---
+++ js/todos.js
@@ -0,0 +1,63 @@
+window.todos = function () {
+	return {
+		todos: [],
+		filter: "all",
+		editedTodo: null,
+		newTodo: "",
+
+		get active() {
+			return this.todos.filter((todo) => !todo.completed);
+		},
+
+		get completed() {
+			return this.todos.filter((todo) => todo.completed);
+		},
+
+		get filteredTodos() {
+			return {
+				all: this.todos,
+				active: this.active,
+				completed: this.completed,
+			}[this.filter];
+		},
+
+		addTodo() {
+			this.todos.push({
+				id: this.todos.length + 1,
+				body: this.newTodo,
+				completed: false,
+			});
+
+			this.newTodo = "";
+		},
+
+		cancelEdit(todo) {
+			todo.body = todo.cachedBody;
+			this.editedTodo = null;
+			delete todo.cachedBody;
+		},
+
+		editComplete(todo) {
+			if (todo.body.trim() === "") {
+				return this.deleteTodo(todo);
+			}
+			this.editedTodo = null;
+		},
+
+		editTodo(todo) {
+			todo.cachedBody = todo.body;
+
+			this.editedTodo = todo;
+		},
+
+		deleteTodo(todo) {
+			let position = this.todos.indexOf(todo);
+
+			this.todos.splice(position, 1);
+		},
+
+		completeTodo(todo) {
+			todo.completed = true;
+		},
+	};
+};
